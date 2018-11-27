c8d23ffbcbf596c1fff30400a11b5929fc61cb26
LauJames <LauJames_work@163.com>
2018-11-27 22:03:05 +0800

    + add inference program

---
+++ infer.py
@@ -0,0 +1,15 @@
+#! /user/bin/evn python
+# -*- coding:utf8 -*-
+
+"""
+
+@Author   : Lau James
+@Contact  : LauJames2017@whu.edu.cn
+@Project  : MVLSTM 
+@File     : infer.py
+@Time     : 18-11-27 下午9:52
+@Software : PyCharm
+@Copyright: "Copyright (c) 2018 Lau James. All Rights Reserved"
+"""
+
+
